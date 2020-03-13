065bad43b2ad54634f15ebcff22efa02e8bdc5fe
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-13 12:22:57 +0100

    Added provider with store

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage.jsx';
 import Landing from './pages/Landing.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ Landing }/>
-        <Route exact path="/login" component={ Login }/>
-        <Route exact path="/register" component={ Register }/>
-        <Route component={ ErrorPage }/>
-      </Switch>
-    </Router>
+    <Provider store={ store }>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Landing }/>
+          <Route exact path="/login" component={ Login }/>
+          <Route exact path="/register" component={ Register }/>
+          <Route component={ ErrorPage }/>
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 
