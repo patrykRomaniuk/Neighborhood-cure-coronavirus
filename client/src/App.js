70ceea56e670a463646ae66f0d2cc657d4ee3a32
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-13 15:17:31 +0100

    Added localstorage verification

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { loadUser } from './actions/auth';
 import ErrorPage from './pages/ErrorPage.jsx';
 import Landing from './pages/Landing.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './middleware/setAuthToken';
+
+if(localStorage.getItem('token')){
+  setAuthToken(localStorage.getItem('token'));
+}
 
 const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  },[])
+
   return (
     <Provider store={ store }>
       <Router>
