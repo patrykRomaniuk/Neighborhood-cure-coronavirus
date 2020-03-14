402eb1e1257e2c71e466b18d186b6aeefc7dc967
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-14 14:57:32 +0100

    Added log out constant

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -2,3 +2,4 @@ export const AUTH_FORM_FAIL = "AUTH_FAIL";
 export const AUTH_FORM_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const USER_LOADED = "USER_LOADED";
+export const LOG_OUT = "LOG_OUT";
