27336233818d9cd27e2678a2538ca8eab88a2382
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-14 14:00:01 +0100

    Made basic constants

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -1,6 +1,4 @@
-export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const REGISTER_FAIL = "REGISTER_FAIL";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAIL = "LOGIN_FAIL";
+export const AUTH_FORM_FAIL = "AUTH_FAIL";
+export const AUTH_FORM_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const USER_LOADED = "USER_LOADED";
