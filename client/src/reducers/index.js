bbec89621a55d71a6158b6ef4179d072137a301b
patrykRomaniuk <patryk-romaniuk1@o2.pl>
2020-03-17 09:38:00 +0100

    Added taskReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import taskReducer from './taskReducer';
 import auth from './authReducer';
 
 export default combineReducers({
-    auth
+    auth,
+    taskReducer
 });
